0fa79320974f20667fe0a6f4bc808143d19642ae
AhmedFaisalCareer <ahmedfaisal.career@gmail.com>
2026-08-02 18:40:02 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 import streamlit as st
 import requests
 from google import genai
-from dotenv import load_dotenv
 
-load_dotenv()
+
+
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 FIREBASE_URL = os.getenv("FIREBASE_URL")
