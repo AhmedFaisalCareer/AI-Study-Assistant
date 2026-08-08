7630a6ee6779d3087386979942d48b699e24b0d0
AhmedFaisalCareer <ahmedfaisal.career@gmail.com>
2026-08-08 17:54:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ Explain everything in simple language suitable for this student.
 
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-3-flash-preview",
         contents=prompt
     )
 
